f5393b593daa7e94bada8ab9249cc0ba9f9690aa
Affaan <supariwalaaffaan@gmail.com>
2026-08-12 19:20:22 +0530

    Add files via upload

---
+++ js/supabaseClient.js
@@ -0,0 +1,8 @@
+
+const SUPABASE_URL = "https://kvdgfqlkjaqvvyxfoqcz.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_9xrQSyCG8G8uFQO_jLTM4Q_6rXXVMcl";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
